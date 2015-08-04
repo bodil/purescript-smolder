8fb4b24675b86b2a8fd734af99385d2834bc3559
Konstantin Zudov <konstantin@anche.no>
2015-08-04 07:19:32 +0300

    Updated to work with 0.7
    * Bumped dependencies
    * Fixed compiler errors
    * Fixed compiler warnings
    * Updated README

--- src/Renderer/Util.purs
+++ src/Renderer/Util.purs
@@ -3,17 +3,22 @@ module Text.Smolder.Renderer.Util
   , Node(..)
   ) where
 
+import Prelude
+
+import Data.Array
 import Data.Maybe
+import Data.List (toList)
 import Data.Tuple
+import Data.Foldable
 
 import qualified Data.Map as Map
 import qualified Text.Smolder.Markup as Markup
 
 data Node
-  = Element String (Map.Map String String) [Node]
+  = Element String (Map.Map String String) (Array Node)
   | Text String
 
-renderMarkup :: forall a. Markup.MarkupM a -> [Node]
+renderMarkup :: forall a. Markup.MarkupM a -> Array Node
 renderMarkup (Markup.Element name (Just children) attrs rest) = 
   Element name (renderAttrs attrs) (renderMarkup children) : renderMarkup rest
 renderMarkup (Markup.Element name Nothing attrs rest) = 
@@ -21,7 +26,7 @@ renderMarkup (Markup.Element name Nothing attrs rest) =
 renderMarkup (Markup.Content text rest) = Text text : renderMarkup rest
 renderMarkup (Markup.Return _) = []
 
-renderAttrs :: [Markup.Attr] -> Map.Map String String
-renderAttrs xs = Map.fromList $ toTuple <$> xs 
+renderAttrs :: Array Markup.Attr -> Map.Map String String
+renderAttrs xs = Map.fromList $ toList $ toTuple <$> xs
   where
   toTuple (Markup.Attr key value) = Tuple key value
