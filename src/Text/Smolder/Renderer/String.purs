17eb90eb878c38e1cc5f8ee04ac254198ba0cbae
Marcin Szamotulski <mszamot@gmail.com>
2017-07-26 10:22:10 +0200

    fix: #25

--- src/Text/Smolder/Renderer/String.purs
+++ src/Text/Smolder/Renderer/String.purs
@@ -26,42 +26,66 @@ escapeMap = fromFoldable
   , "/" /\ "&#x2F;"
   ]
 
-escapeChar :: String -> String
-escapeChar s = fromMaybe s $ lookup s escapeMap
+escapeMIMEMap :: StrMap String
+escapeMIMEMap = fromFoldable
+  [ "&" /\ "&amp;"
+  , "<" /\ "&lt;"
+  , ">" /\ "&gt;"
+  , "\"" /\ "&quot;"
+  , "'" /\ "&#39;"
+  ]
 
-escape :: String -> String
-escape s = joinWith "" (escapeChar <$> (split (Pattern "") s))
+isMIMEAttr :: String -> String -> Boolean
+isMIMEAttr tag attr
+  | attr == "type" && tag == "embed" = true
+  | attr == "type" && tag == "object" = true
+  | attr == "type" && tag == "script" = true
+  | attr == "type" && tag == "source" = true
+  | attr == "type" && tag == "style" = true
+  | otherwise = false
 
 -- url attributes according to:
 -- https://developer.mozilla.org/en-US/docs/Web/HTML/Attributes 
 isURLAttr :: String -> String -> Boolean
-isURLAttr n s
-  | s == "href" && n == "a" = true
-  | s == "href" && n == "area" = true
-  | s == "href" && n == "base" = true
-  | s == "href" && n == "link" = true
-  | s == "src" && n == "audio" = true
-  | s == "src" && n == "embed" = true
-  | s == "src" && n == "iframe" = true
-  | s == "src" && n == "img" = true
-  | s == "src" && n == "input" = true
-  | s == "src" && n == "script" = true
-  | s == "src" && n == "source" = true
-  | s == "src" && n == "track" = true
-  | s == "src" && n == "video" = true
-  | s == "code" && n == "applet" = true
-  | s == "codebase" && n == "applet" = true
-  | s == "data" && n == "object" = true
-  | s == "manifest" && n == "html" = true
-  | s == "poster" && n == "video" = true
+isURLAttr tag attr
+  | attr == "href" && tag == "a" = true
+  | attr == "href" && tag == "area" = true
+  | attr == "href" && tag == "base" = true
+  | attr == "href" && tag == "link" = true
+  | attr == "src" && tag == "audio" = true
+  | attr == "src" && tag == "embed" = true
+  | attr == "src" && tag == "iframe" = true
+  | attr == "src" && tag == "img" = true
+  | attr == "src" && tag == "input" = true
+  | attr == "src" && tag == "script" = true
+  | attr == "src" && tag == "source" = true
+  | attr == "src" && tag == "track" = true
+  | attr == "src" && tag == "video" = true
+  | attr == "code" && tag == "applet" = true
+  | attr == "codebase" && tag == "applet" = true
+  | attr == "data" && tag == "object" = true
+  | attr == "manifest" && tag == "html" = true
+  | attr == "poster" && tag == "video" = true
   | otherwise = false
+
+escape :: StrMap String -> String -> String
+escape m s = joinWith "" (escapeChar <$> (split (Pattern "") s))
+  where
+    escapeChar :: String -> String
+    escapeChar c = fromMaybe c $ lookup c m
+
+escapeAttrValue :: String -> String -> String -> String
+escapeAttrValue tag key value
+  | isURLAttr tag key   = encodeURI value
+  | isMIMEAttr tag key  = escape escapeMIMEMap value
+  | otherwise           = escape escapeMap value
 
 showAttrs :: String -> CatList Attr → String
 showAttrs tag = map showAttr >>> fold
   where showAttr (Attr key value) = " "
           <> key
           <> "=\""
-          <> (if isURLAttr tag key then encodeURI value else escape value)
+          <> escapeAttrValue tag key value
           <> "\""
 
 renderItem :: ∀ e. MarkupM e ~> State String
@@ -72,7 +96,7 @@ renderItem (Element name children attrs _ rest) =
            then ">" <> c <> "</" <> name <> ">"
            else "/>")
   in state \s → Tuple rest $ append s b
-renderItem (Content text rest) = state \s → Tuple rest $ append s $ escape text
+renderItem (Content text rest) = state \s → Tuple rest $ append s $ escape escapeMap text
 renderItem (Empty rest) = pure rest
 
 -- | Render markup as an HTML string.
