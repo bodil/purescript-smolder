3edcf95169f534ac888e88ab249091bc7721b57a
Coot <mszamot@gmail.com>
2017-08-03 01:48:24 +0200

    Url attrs (#31)
    * Text.Smolder.Renderer.String: escapeURL
    
    * Escape URL attributes using `encodeURI`
    
    * fix: #25
    
    * No need to encode ">" (escapeMIMEMap).
    
    * remove import

--- src/Text/Smolder/Renderer/String.purs
+++ src/Text/Smolder/Renderer/String.purs
@@ -8,11 +8,12 @@ import Control.Monad.Free (foldFree)
 import Control.Monad.State (execState, State, state)
 import Data.CatList (CatList)
 import Data.Foldable (fold)
-import Data.Maybe (maybe)
+import Data.Maybe (fromMaybe)
 import Data.StrMap (StrMap, fromFoldable, lookup)
 import Data.String (Pattern(Pattern), joinWith, length, split)
 import Data.Tuple (Tuple(..))
 import Data.Tuple.Nested ((/\))
+import Global (encodeURI)
 import Text.Smolder.Markup (Attr(..), Markup, MarkupM(..))
 
 escapeMap :: StrMap String
@@ -25,25 +26,76 @@ escapeMap = fromFoldable
   , "/" /\ "&#x2F;"
   ]
 
-escapeChar :: String -> String
-escapeChar s = maybe s id $ lookup s escapeMap
+escapeMIMEMap :: StrMap String
+escapeMIMEMap = fromFoldable
+  [ "&" /\ "&amp;"
+  , "<" /\ "&lt;"
+  , "\"" /\ "&quot;"
+  , "'" /\ "&#39;"
+  ]
 
-escape :: String -> String
-escape s = joinWith "" (escapeChar <$> (split (Pattern "") s))
+isMIMEAttr :: String -> String -> Boolean
+isMIMEAttr tag attr
+  | attr == "type" && tag == "embed" = true
+  | attr == "type" && tag == "object" = true
+  | attr == "type" && tag == "script" = true
+  | attr == "type" && tag == "source" = true
+  | attr == "type" && tag == "style" = true
+  | otherwise = false
 
-showAttrs :: CatList Attr → String
-showAttrs = map showAttr >>> fold
-  where showAttr (Attr key value) = " " <> key <> "=\"" <> escape value <> "\""
+-- url attributes according to:
+-- https://developer.mozilla.org/en-US/docs/Web/HTML/Attributes 
+isURLAttr :: String -> String -> Boolean
+isURLAttr tag attr
+  | attr == "href" && tag == "a" = true
+  | attr == "href" && tag == "area" = true
+  | attr == "href" && tag == "base" = true
+  | attr == "href" && tag == "link" = true
+  | attr == "src" && tag == "audio" = true
+  | attr == "src" && tag == "embed" = true
+  | attr == "src" && tag == "iframe" = true
+  | attr == "src" && tag == "img" = true
+  | attr == "src" && tag == "input" = true
+  | attr == "src" && tag == "script" = true
+  | attr == "src" && tag == "source" = true
+  | attr == "src" && tag == "track" = true
+  | attr == "src" && tag == "video" = true
+  | attr == "code" && tag == "applet" = true
+  | attr == "codebase" && tag == "applet" = true
+  | attr == "data" && tag == "object" = true
+  | attr == "manifest" && tag == "html" = true
+  | attr == "poster" && tag == "video" = true
+  | otherwise = false
+
+escape :: StrMap String -> String -> String
+escape m s = joinWith "" (escapeChar <$> (split (Pattern "") s))
+  where
+    escapeChar :: String -> String
+    escapeChar c = fromMaybe c $ lookup c m
+
+escapeAttrValue :: String -> String -> String -> String
+escapeAttrValue tag key value
+  | isURLAttr tag key   = encodeURI value
+  | isMIMEAttr tag key  = escape escapeMIMEMap value
+  | otherwise           = escape escapeMap value
+
+showAttrs :: String -> CatList Attr → String
+showAttrs tag = map showAttr >>> fold
+  where showAttr (Attr key value) = " "
+          <> key
+          <> "=\""
+          <> escapeAttrValue tag key value
+          <> "\""
 
 renderItem :: ∀ e. MarkupM e ~> State String
 renderItem (Element name children attrs _ rest) =
   let c = render children
-      b = "<" <> name <> showAttrs attrs <>
+      b = "<" <> name <> showAttrs name attrs <>
           (if length c > 0
            then ">" <> c <> "</" <> name <> ">"
            else "/>")
   in state \s → Tuple rest $ append s b
-renderItem (Content text rest) = state \s → Tuple rest $ append s $ escape text
+renderItem (Content text rest) = state \s → Tuple rest $ append s $ escape escapeMap text
 renderItem (Empty rest) = pure rest
 
 -- | Render markup as an HTML string.
